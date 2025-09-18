967c04ae9369da92e82cf9e792b85db1a7845787
VISHVESWRA <vishveswravichu@gmail.com>
2025-09-18 10:06:11 +0530

    fix: dotenv

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    port: process.env.PORT,
-    mongoUrl: process.env.MONGO_URL
+  port: process.env.PORT,
+  mongoUrl: process.env.MONGO_URL,
 };
 
 export default config;
